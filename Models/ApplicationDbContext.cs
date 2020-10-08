7aed586ff2ce88ee299fcef9421b762e715ca37c
Mahir Skula <mahir.skula96@hotmail.com>
2020-10-08 12:56:05 +0200

    ExpandingApplicationUser

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace NewsApp.Models
+{
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<News> News { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
